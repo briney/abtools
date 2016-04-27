b03c26c3ff8ddc8c69894a6ba1d489dd48688001
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-04-27 01:02:33 -0700

    update __init__ to work with readthedocs

--- abtools/__init__.py
+++ abtools/__init__.py
@@ -1,21 +1,23 @@
-from _compare import run as compare
-from _correct import run as correct
-from _finder import run as finder
-from _phylogeny import run as phylogeny
-# import _stats as stats
+if not os.environ.get('READTHEDOCS', None):
 
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
+    from _compare import run as compare
+    from _correct import run as correct
+    from _finder import run as finder
+    from _phylogeny import run as phylogeny
+    # import _stats as stats
 
-try:
-    _dist = get_distribution('abtools')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'abtools')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install AbTools before checking the version'
-else:
-    __version__ = _dist.version
+    from pkg_resources import get_distribution, DistributionNotFound
+    import os.path
+
+    try:
+        _dist = get_distribution('abtools')
+        # Normalize case for Windows systems
+        dist_loc = os.path.normcase(_dist.location)
+        here = os.path.normcase(__file__)
+        if not here.startswith(os.path.join(dist_loc, 'abtools')):
+            # not installed, but there is another version that *is*
+            raise DistributionNotFound
+    except DistributionNotFound:
+        __version__ = 'Please install AbTools before checking the version'
+    else:
+        __version__ = _dist.version
