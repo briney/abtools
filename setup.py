8cb631a6ef2389a1291a69550d4021d22d9ea1cb
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-04-27 00:24:49 -0700

    update setup.py for readthedocs, add requirements.txt

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
-from setuptools import setup
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    sys.exit('ERROR: setuptools is required.\n')
+
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    sys.exit('ERROR: pip is required.\n')
+
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+    # Set empty install_requires to get install to work on readthedocs
+    install_requires = []
+else:
+    req_file = 'requirements.txt'
+    try:
+        reqs = parse_requirements(req_file, session=False)
+    except TypeError:
+        reqs = parse_requirements(req_file)
+    install_requires = [str(r.req) for r in reqs]
+
 
 config = {
-    'description': 'Analysis of antibody NGS data',
+    'description': 'Utilities for analysis of antibody NGS data',
     'author': 'Bryan Briney',
     'url': 'www.github.com/briney/abtools/',
     # 'download_url': 'www.github.com/briney/abtools/',
     'author_email': 'briney@scripps.edu',
     'version': '0.1.1',
-    'install_requires': ['nose',
-                         'biopython',
-                         'celery',
-                         'ete2',
-                         'matplotlib',
-                         'numpy',
-                         'nwalign',
-                         'pandas',
-                         'pymongo',
-                         'scikit-bio',
-                         'seaborn'],
+    'install_requires': install_requires,
     'packages': ['abtools'],
     'scripts': ['bin/abcompare',
                 'bin/abcorrect',
