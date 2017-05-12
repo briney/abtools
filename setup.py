68d662a12590e6f0b53aad2fceb87a76d016d5ba
bryanbriney <bryanbriney@dhcp-228-113.scripps.edu>
2017-05-12 14:50:08 -0700

    bump PyPI version to 0.1.7

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ config = {
     'url': 'https://www.github.com/briney/abtools',
     # 'download_url': 'www.github.com/briney/abtools/',
     'author_email': 'briney@scripps.edu',
-    'version': '0.1.6',
+    'version': '0.1.7',
     'install_requires': install_requires,
     'packages': ['abtools'],
     'scripts': ['bin/abcompare',
