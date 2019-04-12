7ee4c52a70d731034e5ecdff9f5c92d44eceaae6
briney <bryanbriney@gmail.com>
2019-04-12 16:52:41 -0700

    Update __init__.py

--- abtools/hiv/__init__.py
+++ abtools/hiv/__init__.py
@@ -1,5 +1,5 @@
-from .bnabs import get_bnab, get_bnabs, get_bnab_dict
+import os
 
-bnAbs = get_bnab_dict()
+from .bnabs import get_bnab, get_bnabs, get_bnab_dict
 
 CATNAP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CATNAP_data')
