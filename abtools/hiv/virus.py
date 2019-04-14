a7870720ec9ff92e3f4c6b210ed95cfcb2da7e79
briney <bryanbriney@gmail.com>
2019-04-13 20:08:26 -0700

    Update virus.py

--- abtools/hiv/virus.py
+++ abtools/hiv/virus.py
@@ -22,10 +22,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+import os
+import sys
 
+import pandas as pd
+
+from abutils.core.sequence import read_fasta
 from abutils.utils.decorators import lazy_property
 
 from . import CATNAP_PATH
+
+if sys.version_info[0] > 2:
+    STR_TYPES = [str, ]
+else:
+    STR_TYPES = [str, unicode]
 
 
 class Virus():
@@ -36,6 +46,11 @@ class Virus():
         self.raw = virus_data
         self.aligned_nt_sequence = nt_sequence
         self.aligned_aa_sequence = aa_sequence
+
+    def __eq__(self, other):
+        if other in [self.name] + self.aliases:
+            return True
+        return False
     
     @property
     def nt_sequence(self):
@@ -114,5 +129,25 @@ class Virus():
         return self.raw['# of Abs tested']
 
 
+def get_virus(virus):
+    return get_viruses([virus, ])
 
-# def get_viruses()
+
+def get_viruses(viruses=None):
+    if type(viruses) in STR_TYPES:
+        viruses = [viruses, ]
+    # read the virus file
+    virus_df = pd.read_csv(os.path.join(CATNAP_PATH, 'viruses.txt'), sep='\t')
+    virus_aa = read_fasta(os.path.join(CATNAP_PATH, 'virus_seqs_aa.fasta'))
+    virus_nt = read_fasta(os.path.join(CATNAP_PATH, 'virus_seqs_nt.fasta'))
+    if viruses is None:
+        viruses = virus_df['Virus name'].unique()
+    virus_data = []
+    for v in viruses:
+        vdata = virus_df[virus_df['Virus name'] == v].to_dict()
+        vaa = [s for s in virus_aa if s.id == v][0].sequence
+        vnt = [s for s in virus_nt if s.id == v][0].sequence
+        virus_data.append(Virus(vdata, nt_sequence=vnt, aa_sequence=vaa))
+    if len(virus_data) == 1:
+        return virus_data[0]
+    return virus_data
